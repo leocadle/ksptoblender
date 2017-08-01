083c3daf84d0d339dfbec7f1b15e2966f31ab119
Leo <leo@cadle.org>
2017-08-01 18:27:03 +1000

    Added changed files

---
+++ README.txt
@@ -0,0 +1,44 @@
+         A         
+        / \       
+       | 0 |       
+       |___|       
+       |___|       
+       |KSP|       
+      /|   |\     
+     /| \_/ |\   
+    /_|  W  |_\   
+       @WWW@       
+     @@WWWWW@@     
+    @ @@@@ @@@ @   
+  @   @  @@  @   @ 
+ 
+
+KSPBLENDER .95
+3/1/2015
+SPENCER ARRASMITH
+https://github.com/Dasoccerguy/io_kspblender
+DASOCCERGUY39@GMAIL.COM
+
+Thanks for checking out the readme!
+
+
+
+INITAL SETUP
+
+To get this working, you will need to download 2 Blender addons, enable them, and change kspdir to the installation location of Kerbal Space Program
+
+1. Click "Download ZIP," unzip the archive, then place the whole folder into the Blender Addons folder (Blender Foundation\Blender\2.73\scripts\addons)
+2. Download the .mu importer from https://github.com/taniwha-qf/io_object_mu, and place the unzipped folder in the same location
+3. Find KSP.exe (look in Steam\Steamapps\common\Kerbal Space Program)
+4. Copy the directory name
+5. Paste it directly into kspdir.txt and save
+6. In Blender, press CTRL+ALT+U, go to Addons, and enable this addon. It is called "Import-Export: KSPBlender .craft Import"
+7. Either click "Save User Settings" or come back and enable the addon every time you want to use it.
+8. Enjoy!
+
+
+
+MOD PARTS
+
+Thanks to Cptman's hard work, there is now a function to procedurally find parts... which means that every mod is now supported.
+However, many parts may import with the wrong scale or be slightly misplaced. Please report any scaling or location issues!
---
+++ right_scale.py
@@ -0,0 +1,73 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+from mathutils import Vector
+
+def make():
+    '''Makes the right_scale dictionary to fix scaling'''
+
+    right_scale = {}
+    
+    right_scale['Mark1Cockpit']             = Vector((1.25,1.25,1.25))
+    right_scale['Mark2Cockpit']             = Vector((1.25,1.25,1.25))
+    right_scale['fuelTank']                 = Vector((1.25,1.25,1.25))
+    right_scale['fuelTankSmall']            = Vector((1.25,1.25,1.25))
+    right_scale['fuelTank.long']            = Vector((1.25,1.25,1.25))
+    right_scale['fuelTank_long']            = Vector((1.25,1.25,1.25))
+    right_scale['noseConeAdapter']          = Vector((1.25,1.25,1.25))
+    right_scale['standardNoseCone']         = Vector((1.25,1.25,1.25))
+    right_scale['liquidEngine']             = Vector((1.25,1.25,1.25))
+    right_scale['liquidEngine2']            = Vector((1.25,1.25,1.25))
+    right_scale['liquidEngine3']            = Vector((1.25,1.25,1.25))
+    right_scale['science.module']           = Vector((1.25,1.25,1.25))
+    right_scale['fuelTank.long']            = Vector((1.25,1.25,1.25))
+    right_scale['Mk1Fuselage']              = Vector((1.25,1.25,1.25))
+    right_scale['Mk1FuselageStructural']    = Vector((1.25,1.25,1.25))
+    right_scale['Mk1IntakeFuselage']        = Vector((1.25,1.25,1.25))
+    right_scale['nacelleBody']              = Vector((1.25,1.25,1.25))
+    right_scale['radialEngineBody']         = Vector((1.25,1.25,1.25))
+    right_scale['turboFanEngine']           = Vector((1.25,1.25,1.25))
+    right_scale['CircularIntake']           = Vector((1.25,1.25,1.25))
+    right_scale['toroidalAerospike']        = Vector((1.25,1.25,1.25))
+    right_scale['ramAirIntake']             = Vector((1.25,1.25,1.25))
+    right_scale['JetEngine']                = Vector((1.25,1.25,1.25))
+    right_scale['RAPIER']                   = Vector((1.25,1.25,1.25))
+    right_scale['winglet']                  = Vector((1.25,1.25,1.25))
+    right_scale['R8winglet']                = Vector((1.25,1.25,1.25))
+    right_scale['solidBooster']             = Vector((1.25,1.25,1.25))
+
+    #Kethane
+    right_scale['kethane.1m.converter']     = Vector((1.25,1.25,1.25))
+    right_scale['kethane.2m.converter']     = Vector((1.25,1.25,1.25))
+    right_scale['kethane.generator']        = Vector((.666,.666,.666))
+    right_scale['kethane.heavyDrill']       = Vector((.00625,.00625,.00625))
+    right_scale['kethane.highGain']         = Vector((1.5,1.5,1.5))
+    right_scale['kethane.kerbalBlender']    = Vector((1.25,1.25,1.25))
+    #right_scale['kethane.radialDrill']      = Vector(())
+    right_scale['kethane.sensor.1m']        = Vector((1.25,1.25,1.25))
+    right_scale['kethane.smallDrill']       = Vector((.02,.02,.02))
+    #right_scale['kethane.tank1mLarge']      = Vector(())
+    right_scale['kethane.tank1mStandard']   = Vector((.0125,.0125,.0125))
+    right_scale['kethane.tank2mExtralarge'] = Vector((1.2,1.2,1.2))
+    right_scale['kethane.tank2mLarge']      = Vector((1.2,1.2,1.2))
+    right_scale['kethane.tank2mMedium']     = Vector((1.2,1.2,1.2))
+    right_scale['kethane.tank2mSmall']      = Vector((1.2,1.2,1.2))
+    right_scale['kethane.tankExternal']     = Vector((1.25,1.25,1.25))
+    right_scale['kethane.turbine']          = Vector((.666,.666,.666))
+
+    return right_scale
